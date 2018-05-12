f8a48debfe7c499f815faf62b7a69d9f1392ccb9
Oliver Rýdzi <orydzi@gmail.com>
2018-05-12 20:43:27 +0200

    ES5 syntax

--- libs/http.js
+++ libs/http.js
@@ -17,24 +17,23 @@ var post = function (url, body) {
     };
     return request(obj);
 };
-
 var request = function(obj) {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
         xhr.open(obj.method || "GET", obj.url);
         if (obj.headers) {
-            Object.keys(obj.headers).forEach(key => {
+            Object.keys(obj.headers).forEach(function(key) {
                 xhr.setRequestHeader(key, obj.headers[key]);
             });
         }
-        xhr.onload = () => {
+        xhr.onload = function() {
             if (xhr.status >= 200 && xhr.status < 300) {
                 resolve(xhr.response);
             } else {
                 reject(xhr.statusText);
             }
         };
-        xhr.onerror = () => reject(xhr.statusText);
+        xhr.onerror = function() { return reject(xhr.statusText); }
         xhr.send(obj.body);
     });
 };
--- libs/qrcode.js
+++ libs/qrcode.js
@@ -21,32 +21,32 @@ var addCssRule = function() {
 var createDeaultQRContainer = function() {
 	var canvas = document.createElement("canvas");
 	canvas.className = "qrcode";
-	var canvasStyle = `box-shadow: 2px 2px 12px lightgray;`;
+	var canvasStyle = "box-shadow: 2px 2px 12px lightgray;";
 	addCssRule(".qrcode", canvasStyle);
 
 	var qrcontainer = document.createElement("div");
 	qrcontainer.className = "qrcode-container";
-	var style = `text-align: center;
-    background-color: #fff0;
-    border-radius: 20px;
-    width: 300px;
-    height: 300px;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%,-50%);`;
+  var style = ("text-align: center;\
+    background-color: #fff0;\
+    border-radius: 20px;\
+    width: 300px;\
+    height: 300px;\
+    position: absolute;\
+    left: 50%;\
+    top: 50%;\
+    transform: translate(-50%,-50%);");
 	addCssRule(".qrcode-container", style);
 	qrcontainer.appendChild(canvas);
 
 	var background = document.createElement("div");
 	background.className = "qrcode-background";
-	style = `position:absolute;
-	left:0;
-	top:0;
-	z-index:100;
-	height:100%;
-	width:100%;
-	background-color: rgba(0, 0, 0, 0.4);`;
+	style = "position:absolute;\
+	left:0;\
+	top:0;\
+	z-index:100;\
+	height:100%;\
+	width:100%;\
+	background-color: rgba(0, 0, 0, 0.4);";
 	addCssRule(".qrcode-background", style);
 	background.appendChild(qrcontainer);
 
